30b6370cde41d61a7b4562c8891674af5d78ac9c
ivanaradi <39125701+ivanaradi@users.noreply.github.com>
2018-06-04 12:03:34 +0200

    dodate korisnikove vesti

---
+++ src/java/model/Vidljivost.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Korisnik
+ */
+public enum Vidljivost {
+    ADMIN (1),
+    SVI (2),
+    INVESTITORI (3),
+    STARTAPI (4),
+    CUSTOM (5);
+    
+  
+    private final int sifra;
+
+    Vidljivost(int sifra) {
+        this.sifra = sifra;
+    }
+    
+    public int getSifra() {
+        return this.sifra;
+    }
+    
+}
+
